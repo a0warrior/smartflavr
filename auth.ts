228a53eac958210c5ea2954f598654f59a54af80
a0warrior <aarynjwarrior@gmail.com>
2026-06-19 16:00:14 -0500

    Add trustHost to NextAuth config

--- auth.ts
+++ auth.ts
@@ -3,6 +3,7 @@ import Google from "next-auth/providers/google"
 import pool from "@/lib/db"
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
+  trustHost: true,
   providers: [Google({
     clientId: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
